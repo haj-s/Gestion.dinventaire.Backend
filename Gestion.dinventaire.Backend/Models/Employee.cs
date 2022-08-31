4ed5985b5d492f74628c0bfd8253c2aa00662223
haj-s <72891004+haj-s@users.noreply.github.com>
2022-08-31 11:28:20 +0200

    je viens de créer une classe employee

---
+++ Gestion.dinventaire.Backend/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Gestion.dinventaire.Backend.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string  username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public bool IsActif { get; set; }
+    }
+}
